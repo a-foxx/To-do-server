1236fe1f6b8a62b002ba1af2c14af7feab556f5b
Mister Fox <alanfox70@gmail.com>
2023-08-08 22:25:58 +0100

    vercel

--- functions/db.js
+++ functions/db.js
@@ -13,6 +13,15 @@ const proConfig = {
     connectionString: process.env.DATABASE_URL //heroku
 }
 
-const pool = new Pool(devConfig)
+//vercel
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  })
+
+pool.connect((err) => {
+    if (err) throw err
+    console.log('connected to postgreSQL successfully')
+})
+// const pool = new Pool(devConfig)
 
 module.exports = pool;
